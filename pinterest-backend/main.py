6b7352e4ca6f58f10a93088e07de6caeb1498117
Sofia Imelda <sgonz114@itam.mx>
2026-05-16 22:38:18 -0600

    actualizacion access keys

--- pinterest-backend/main.py
+++ pinterest-backend/main.py
@@ -1,51 +1,15 @@
 from fastapi import FastAPI
-from pydantic  import BaseModel
-from fastapi import Header
-import requests
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-ACCESS_KEY = os.getenv("ACCESS_KEY")
-print(ACCESS_KEY)
-app=FastAPI()
-posts=[]
-
-class Post (BaseModel):
-    id:int
-    title:str
-    image:str
-    tags:list[str]
-    user:str
-
-@app.get("/posts")
-def get_posts():
-    return posts   
-
-@app.post("/posts")
-def create_post(post: Post,usuario:str=Header(...)):
-   print(usuario)
-   posts.append(post)
-   return post
-
-@app.delete("/posts/{id}")
-def delete_post(id:int):
-    #variable global para que no solo cambie localmente
-    global posts
-    #[nuevo_elemento for elemento in lista if condición]
-    posts=[post for post in posts if post.id !=id]
-    return {"message": "Post eliminado"}
-
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post):
-    global posts
-#si el post tiene ese id,reemplázalo.Si no,déjalo igual
-
 from pydantic import BaseModel
 from fastapi import Header
 from fastapi.middleware.cors import CORSMiddleware
 import requests
+from dotenv import load_dotenv
+import os
+
+# CARGAR .ENV
+load_dotenv()
+
+ACCESS_KEY = os.getenv("ACCESS_KEY")
 
 app = FastAPI()
 
@@ -111,41 +75,12 @@ def update_post(id: int, updated_post: Post):
 
     updated_post.id = id
 
-
     posts = [
         updated_post if post.id == id else post
         for post in posts
     ]
 
     return updated_post
-
-
-    
-@app.get("/imagenes")
-def get_imagenes():
-
-    headers = {
-        "Authorization": f"Client-ID {ACCESS_KEY}" ,
-         "Accept-Version": "v1"
-    }
-
-    response = requests.get(
-        "https://api.unsplash.com/photos",
-        headers=headers
-    )
-
-    data = response.json()
-
-    imagenes = []
-
-    for img in data:
-        imagenes.append({ 
-            "id": img["id"],
-            "url": img["urls"]["small"],
-            "autor": img["user"]["name"]
-        })
-
-    return imagenes
 
 
 # API EXTERNA UNSPLASH
@@ -155,7 +90,7 @@ def get_imagenes():
     url = "https://api.unsplash.com/photos/random?count=9"
 
     headers = {
-        "Authorization": "Client-ID QmbROGjkwsAqByT3OIgID5FoHuIvMaQ9APiq2D7qR4g"
+        "Authorization": f"Client-ID {ACCESS_KEY}"
     }
 
     response = requests.get(url, headers=headers)
